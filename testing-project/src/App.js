bb8a4962d6e0a2406f731b52f47e0cee004e201b
AbdelIdir <homeworkoracle@protonmail.com>
2019-12-05 14:14:05 +0100

    created display and button

--- testing-project/src/App.js
+++ testing-project/src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
+  const Container = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 100vh;
+    background:beige;
+  `;
+  const Button = styled.div`
+    width: 200px;
+    padding: 40px;
+    background: purple;
+    border-radius: 15px;
+  `;
+  const Display = styled.div`
+    width: 300px;
+    padding: 30px;
+    background: green;
+  `;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Button>hi</Button>
+        <Display></Display>
+      </Container>
     </div>
   );
 }

8e0c9216307498859a438b47e0955e10a0f967b2
AbdelIdir <homeworkoracle@protonmail.com>
2019-12-05 18:51:09 +0100

    testted if the display shows up

--- testing-project/src/App.test.js
+++ testing-project/src/App.test.js
@@ -11,10 +11,14 @@ afterEach(rtl.cleanup)
 it('shows that the word is present ', () => {
   // step 3 - mount the component we want to test
   const wrapper = rtl.render(<App />)
+  wrapper.debug()
   // step 4 - capture the element of interest BY "READABLE" PROPERTY
   // we normally care about the readable text
   // or the aria attributes (screen readers)
   const learnReactElement = wrapper.queryByText(/balls/i)
+
+  const dis = wrapper.queryByText(/display/i)
+  
   // step 5 - assert that the element is there
   expect(learnReactElement).toBeInTheDocument()
 });
